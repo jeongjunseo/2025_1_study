85ba1cde6bba757a4c10a0d94913e0117e6a3f03
이정주 <leezungzoo@ijeongjuui-MacBookAir.local>
2025-04-20 21:41:58 +0900

    [week3] leezungzoo

---
+++ leezungzoo/week3/boj_18258.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 2000000
+
+int queue[MAX];
+int front_idx = 0;
+int back_idx = 0;
+
+void push(int x) {
+    queue[back_idx++] = x;
+}
+
+int pop() {
+    if (front_idx == back_idx) return -1;
+    return queue[front_idx++];
+}
+
+int size() {
+    return back_idx - front_idx;
+}
+
+int empty() {
+    return (front_idx == back_idx) ? 1 : 0;
+}
+
+int front() {
+    if (empty()) return -1;
+    return queue[front_idx];
+}
+
+int back() {
+    if (empty()) return -1;
+    return queue[back_idx - 1];
+}
+
+int main() {
+    int n, x;
+    char cmd[10];
+    scanf("%d", &n);
+
+    while (n--) {
+        scanf("%s", cmd);
+        if (strcmp(cmd, "push") == 0) {
+            scanf("%d", &x);
+            push(x);
+        } else if (strcmp(cmd, "pop") == 0) {
+            printf("%d\n", pop());
+        } else if (strcmp(cmd, "size") == 0) {
+            printf("%d\n", size());
+        } else if (strcmp(cmd, "empty") == 0) {
+            printf("%d\n", empty());
+        } else if (strcmp(cmd, "front") == 0) {
+            printf("%d\n", front());
+        } else if (strcmp(cmd, "back") == 0) {
+            printf("%d\n", back());
+        }
+    }
+
+    return 0;
+}
