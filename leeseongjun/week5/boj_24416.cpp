014cee413bc308026800f9b123ba0b44464d3db5
djkdb <tjdwns2121@naver.com>
2025-05-18 00:48:58 +0900

    (#23) week5 leeseongjun

--- leeseongjun/week5/boj_24416.cpp
+++ leeseongjun/week5/boj_24416.cpp
@@ -1,28 +1,33 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
+
+int count_code1 = 0; 
+int count_code2 = 0; 
+
+int fib(int n) {
+    if (n == 1 || n == 2) {
+        return 1;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+
+int fibonacci(int n) {
+    int f[41];
+    f[1] = f[2] = 1;
+    for (int i = 3; i <= n; i++) {
+        f[i] = f[i - 1] + f[i - 2];
+        count_code2++;
+    }
+    return f[n];
+}
 
 int main() {
     int n;
     cin >> n;
 
-    vector<int> A(n);
-    for (int i = 0; i < n; i++)
-        cin >> A[i];
+    fib(n);       
+    fibonacci(n);  
 
-    vector<int> dp(n, 1)
-
-    for (int i = 1; i < n; i++) {
-        for (int j = 0; j < i; j++) {
-            if (A[j] < A[i]) {
-                dp[i] = max(dp[i], dp[j] + 1);
-            }
-        }
-    }
-
-    int answer = *max_element(dp.begin(), dp.end());
-    cout << answer << endl;
-
+    cout << count_code1 << " " << count_code2 << "\n";
     return 0;
 }
