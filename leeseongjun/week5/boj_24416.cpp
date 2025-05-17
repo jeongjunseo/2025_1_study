3b453ed8502930528a536f3155ca3dafe1d5d154
djkdb <tjdwns2121@naver.com>
2025-05-18 00:42:47 +0900

    (#23) week5 leeseongjun

---
+++ leeseongjun/week5/boj_24416.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> A(n);
+    for (int i = 0; i < n; i++)
+        cin >> A[i];
+
+    vector<int> dp(n, 1)
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (A[j] < A[i]) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+
+    int answer = *max_element(dp.begin(), dp.end());
+    cout << answer << endl;
+
+    return 0;
+}
